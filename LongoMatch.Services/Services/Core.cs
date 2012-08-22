1c2691c1e8739fe441dccb7f33e8cf558be5d7f4
Andoni Morales Alastruey <ylatuya@gmail.com>
2012-08-22 19:05:22 +0200

    Find the prefix in the same way for all the platforms

--- LongoMatch.Services/Services/Core.cs
+++ LongoMatch.Services/Services/Core.cs
@@ -154,11 +154,7 @@ namespace LongoMatch.Services
 		private static void SetupBaseDir() {
 			string home;
 			
-			if (Environment.OSVersion.Platform == PlatformID.Win32NT) {
-				Config.baseDirectory = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory,"../");
-			}
-			else
-				Config.baseDirectory = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory,"../../");
+			Config.baseDirectory = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory,"../../");
 			
 			/* Check for the magic file PORTABLE to check if it's a portable version
 			 * and the config goes in the same folder as the binaries */
